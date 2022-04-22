2df904b3f7e8b1a8e43094ba18f31209a12271e8
Lucas Pinard <58945530+lucasP243@users.noreply.github.com>
2022-04-22 20:36:50 +0200

    Added database handler header guard

--- Partie_Centralisee/user_database_handler.h
+++ Partie_Centralisee/user_database_handler.h
@@ -1,5 +1,10 @@
+#ifndef USER_DATABASE_HANDLER_H
+#define USER_DATABASE_HANDLER_H
+
 extern void user_database_open();
 
 extern void user_database_close();
 
 extern int user_database_request();
+
+#endif
